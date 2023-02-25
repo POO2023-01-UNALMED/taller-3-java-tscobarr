18360dc9000d47a1f86fcefb5fb292ae0cc67d72
tscobarr <124937392+tscobarr@users.noreply.github.com>
2023-02-25 11:41:40 -0500

    Test

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,112 @@
+package taller3.televisores;
+
+public class TV {
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    private Control control;
+    private static int numTV;
+    
+    //Constructor
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+        numTV++;
+    	}
+    
+    //Marca
+    public Marca getMarca() {
+        return marca;
+    }
+    
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    
+    //Control
+    public Control getControl() {
+        return control;
+    }
+    
+    public void setControl(Control control) {
+        this.control = control;
+    }
+    
+    //Precio
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    //Volumen
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        if (estado && volumen >= 0 && volumen <= 7) {
+            this.volumen = volumen;
+        }
+    }
+    
+    //Canal
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        if(estado && canal >= 1 && canal <= 120) {
+            this.canal = canal;
+        }
+    }
+    
+    //numTV
+    public static int getNumTV() {
+        return numTV;
+    }
+    
+    //Estado apagado o encendido
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void turnOn() {
+        estado = true;
+    }
+
+    public void turnOff() {
+        estado = false;
+    }
+    
+    //Modificar canales
+    public void canalUp() {
+        if(estado && canal < 120) {
+            canal++;
+        }
+    }
+
+    public void canalDown() {
+        if(estado && canal > 0) {
+            canal--;
+        }
+    }
+    
+    //Modificar volumen
+    public void volumenUp() {
+        if(estado && volumen < 7) {
+            volumen++;
+        }
+    }
+    
+    public void volumenDown() {
+        if(estado && volumen > 0) {
+            volumen--;
+        }
+    }
+
+}
